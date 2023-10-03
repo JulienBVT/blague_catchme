3bf926393a5c51664c61db044d54701307bd12c9
Julien <julien.brevet01@gmail.com>
2023-10-03 12:39:45 +0200

    rajout timeout

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const button = document.getElementById('catchMeBtn');
+const video = document.getElementById('myVideo');
 
 button.addEventListener('mouseover', () => {
   const maxX = window.innerWidth - button.clientWidth;
   const maxY = window.innerHeight - button.clientHeight;
 
-  const newX = Math.floor(Math.random() * maxX);
-  const newY = Math.floor(Math.random() * maxY);
+  setTimeout(() => {
+    const newX = Math.floor(Math.random() * maxX);
+    const newY = Math.floor(Math.random() * maxY);
 
-  button.style.left = `${newX}px`;
-  button.style.top = `${newY}px`;
+    button.style.left = `${newX}px`;
+    button.style.top = `${newY}px`;
+
+  }, 100);
+});
+
+button.addEventListener('click', () => {
+  video.style.display = 'block';
+  video.play();
 });
